6c16e651ab6ca42ef80852f9ce1ee2e2660b7070
Tel <cmeeinspire@gmail.com>
2015-05-05 12:50:27 -0400

    Cmeeinspire First Commit

---
+++ source/js/script.js
@@ -0,0 +1,65 @@
+$(document).ready(function(){
+
+	$('body').fadeIn(500);
+
+	$('.toggle-menu').click(function(){
+		$('.nav-toggle').slideToggle(600);
+	});
+
+	$('.toggle-dropdown').click(function(){
+		$('.dropdown').slideToggle(600);
+		$('.toggle-dropdown').toggleClass('dropdown-active');
+	});
+
+	var speed = 500;		//fade speed
+	var autoswitch = false;		//autoslide option
+	var autoswitch_speed =5000; //auto slide speed 
+	//add intiial active class
+
+	$('.slide').first().addClass('active-slide');
+	$('.slide-copy').first().addClass('active-slide');
+	$('.slide').hide();
+	$('.slide-copy').hide();
+	$('.active-slide').show();
+
+	$('#next').on('click', nextSlide);
+
+	$('#prev').on('click', prevSlide);
+
+	if(autoswitch == true){
+		setInterval(nextSlide, autoswitch_speed)
+	}
+
+	// Switch to next slide
+
+	function nextSlide(){
+		$('.active-slide').removeClass('active-slide').addClass('oldactive');
+		if($('.oldactive').is(':last-child')){
+			$('.oldactive').next().addClass('active-slide');
+			$('.slide').first().addClass('active-slide');
+			$('.slide-copy').first().addClass('active-slide');
+
+		} else {
+			$('.oldactive').next().addClass('active-slide');
+			}
+			$('.oldactive').removeClass('oldactive');
+			$('.slide').fadeOut(speed);
+			$('.slide-copy').fadeOut(speed);
+			$('.active-slide').fadeIn(600);
+	}
+
+	function prevSlide(){
+		$('.active-slide').removeClass('active-slide').addClass('oldactive');
+		if($('.oldactive').is(':first-child')){
+			$('.slide').last().addClass('active-slide');
+			$('.slide-copy').last().addClass('active-slide');
+		} else {
+			$('.oldactive').prev().addClass('active-slide');
+			}
+			$('.oldactive').removeClass('oldactive');
+			$('.slide').fadeOut(speed);
+			$('.slide-copy').fadeOut(speed);
+			$('.active-slide').fadeIn(600);
+	}
+
+});
