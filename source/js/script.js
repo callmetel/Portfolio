ef0136ad4af4ca65c2e8484e45ce141e5f4b2dda
Tel <cmeeinspire@gmail.com>
2015-05-06 15:58:37 -0400

    About Scroll Animation

--- source/js/script.js
+++ source/js/script.js
@@ -1,6 +1,11 @@
 $(document).ready(function(){
 
+// Body Animation
+
 	$('body').fadeIn(500);
+
+
+// Nav Toggle Animation
 
 	$('.toggle-menu').click(function(){
 		$('.nav-toggle').slideToggle(600);
@@ -10,6 +15,8 @@ $(document).ready(function(){
 		$('.dropdown').slideToggle(600);
 		$('.toggle-dropdown').toggleClass('dropdown-active');
 	});
+
+// Carousel Functionality
 
 	var autoswitch = true;		//autoslide option
 	var autoswitch_speed =8000; //auto slide speed 
@@ -26,25 +33,44 @@ $(document).ready(function(){
 		setInterval(nextSlide, autoswitch_speed)
 	}
 
-
 	function nextSlide(){
 
-
 		var $active = $('.is-active');
-		var $firstSlide = $('.slide').first();
+		var	$firstSlide = $('.slide').first();
 		var $firstSlideCopy = $('.slide-copy').first();
 
-		var next = $active.is(':last-child') ? ($active.removeClass('is-active'), $firstSlide.addClass('is-active'), $firstSlideCopy.addClass('is-active')) :  $active.removeClass('is-active').next().addClass('is-active');
 
+		var	next = $active.is(':last-child') ? (
+			$active.removeClass('is-active'), $firstSlide.addClass('is-active'), 
+			$firstSlideCopy.addClass('is-active')) :  $active.removeClass('is-active').next().addClass('is-active');
 	}
 
 	function prevSlide(){
 
-		var $active = $('.is-active');
-		var $lastSlide = $('.slide').last();
-		var $lastSlideCopy = $('.slide-copy').last();
-
-		var prev = $active.is(':first-child') ? ($active.removeClass('is-active'), $lastSlide.addClass('is-active'), $lastSlideCopy.addClass('is-active')) :  $active.removeClass('is-active').prev().addClass('is-active');
-
+		var $active = $('.is-active'),
+			$lastSlide = $('.slide').last(),
+			$lastSlideCopy = $('.slide-copy').last(),
+			prev = $active.is(':first-child') ? ($active.removeClass('is-active'), $lastSlide.addClass('is-active'), $lastSlideCopy.addClass('is-active')) :  $active.removeClass('is-active').prev().addClass('is-active');
 	}
+
+// Home Page Fade On Scroll Anmiation
+
+	$(window).scroll(function(e) {
+    var pos = $(window).scrollTop(),
+    	headHeight = $('#heading').height();
+    	aboutHeight = ($('#about').outerHeight( false )) + ($('#heading').outerHeight( false ));
+
+    if (pos > headHeight && pos < aboutHeight) {
+        $('#about').addClass('is-visible');
+        $('#about').addClass('fadeIn-scaleUp');
+
+    }
+    else{
+    	$('#about').addClass('is-hidden');
+    	$('#about').removeClass('is-visible');
+    	$('#about').removeClass('fadeIn-scaleUp');
+    }
+    console.log(headHeight);
+	console.log(aboutHeight);
+	});
 });
