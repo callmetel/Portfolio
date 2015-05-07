8d04e2cb7359f56f08b71fe0d88988bdfd52b889
Tel <cmeeinspire@gmail.com>
2015-05-07 10:26:16 -0400

    Ease Scroll & Draw Animations

--- source/js/script.js
+++ source/js/script.js
@@ -40,17 +40,31 @@ $(document).ready(function(){
 		var $firstSlideCopy = $('.slide-copy').first();
 
 
-		var	next = $active.is(':last-child') ? (
-			$active.removeClass('is-active'), $firstSlide.addClass('is-active'), 
-			$firstSlideCopy.addClass('is-active')) :  $active.removeClass('is-active').next().addClass('is-active');
+		if ($active.is(':last-child')) {
+			$active.removeClass('is-active'); 
+			$firstSlide.addClass('is-active');
+			$firstSlideCopy.addClass('is-active');
+		}
+		else{
+			$active.removeClass('is-active').next().addClass('is-active');
+		}
 	}
 
 	function prevSlide(){
 
 		var $active = $('.is-active'),
 			$lastSlide = $('.slide').last(),
-			$lastSlideCopy = $('.slide-copy').last(),
-			prev = $active.is(':first-child') ? ($active.removeClass('is-active'), $lastSlide.addClass('is-active'), $lastSlideCopy.addClass('is-active')) :  $active.removeClass('is-active').prev().addClass('is-active');
+			$lastSlideCopy = $('.slide-copy').last();
+
+			if ($active.is(':first-child')){
+				$active.removeClass('is-active');
+				$lastSlide.addClass('is-active'); 
+				$lastSlideCopy.addClass('is-active');
+
+			}
+			else{
+				$active.removeClass('is-active').prev().addClass('is-active');
+			}  
 	}
 
 // Home Page Fade On Scroll Anmiation
@@ -66,7 +80,7 @@ $(document).ready(function(){
     	headAboutHeightTotal = ($about.outerHeight( true )) + ($heading.outerHeight( true )),
     	contactHeight = (headAboutHeightTotal) - (($about.outerHeight( false ))/2);
 
-    // About Scroll Animation
+    // About Animation
     if (pos > headHeight && pos < aboutHeight) {
         $about.addClass('is-visible');
         $about.addClass('fadeIn-scaleUp');
@@ -78,6 +92,7 @@ $(document).ready(function(){
     	$about.removeClass('fadeIn-scaleUp');
     }
 
+    // Contact Animation
     if (pos > contactHeight) {
     	$contact.addClass('is-visible');
         $contact.addClass('fadeIn-scaleUp');
@@ -90,4 +105,74 @@ $(document).ready(function(){
     }
 
 	});
+	//Ease Scrolling
+
+	$(function() {
+  	$('a[href*=#]:not([href=#])').on('click', (function() { //get the 'a' anchor with a # but not ones with only a #
+    if (location.pathname.replace(/^\//,'') === this.pathname.replace(/^\//,'') && location.hostname === this.hostname) { //is the location of the pathname & hostname = to this pathname & hostname?
+      var target = $(this.hash); //get the link; section id
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']'); //is the target length = to target? if not slice the target length down to 1
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  }));
+});
+
+// Draw SVG Animation
+
+var $twitter = $('#tw'),
+	$drawTwitter = $('.drawTw'),
+	$facebook = $('#fb'),
+	$drawFacebook = $('.drawFb'),
+	$youtube = $('#yt'),
+	$drawYoutube = $('.drawYt'),
+	$mail = $('#ml'),
+	$drawMail = $('.drawMl'),
+	$arrow = $('.drawThis');
+
+
+
+	  // Draw Logo
+	  TweenMax.from('.grow', 5, {drawSVG:0, rotation: 560, delay:0.8});
+
+	  // Draw Arrow
+ 	  $arrow.mouseover(function (e){    
+          TweenMax.from($(this), 1, {drawSVG:0});
+      });
+      $arrow.mouseout(function (e){    
+          TweenMax.to($(this), 1, {drawSVG:100});
+      });
+
+      // Draw Icons
+      $twitter.mouseover(function (e){    
+          TweenMax.to($drawTwitter, 1, {drawSVG:0});
+      });
+      $twitter.mouseout(function (e){    
+          TweenMax.to($drawTwitter, 1, {drawSVG:130});
+      });
+      $facebook.mouseover(function (e){    
+          TweenMax.to($drawFacebook, 1, {drawSVG:0});
+      });
+      $facebook.mouseout(function (e){    
+          TweenMax.to($drawFacebook, 1, {drawSVG:100});
+      });
+      $youtube.mouseover(function (e){    
+          TweenMax.to($drawYoutube, 1, {drawSVG:0});
+      });
+      $youtube.mouseout(function (e){    
+          TweenMax.to($drawYoutube, 1, {drawSVG:100});
+      });
+      $mail.mouseover(function (e){    
+          TweenMax.to($drawMail, 1, {drawSVG:0});
+      });
+      $mail.mouseout(function (e){    
+          TweenMax.to($drawMail, 1, {drawSVG:130});
+      });
+
+
+
 });
